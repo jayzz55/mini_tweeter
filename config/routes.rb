a0d3c8681b0da61f5e8bbfe3b3c9d2def45083e9
Jaya Wijono from Dot Kingdom <jayzzwijono@yahoo.com>
2015-07-20 01:53:08 +1000

    refactor code, add integration test, and finish add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-  resources :account_activation, only: [:edit]
+  resources :account_activations, only: [:edit]
 end
