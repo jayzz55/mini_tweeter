624e03995b623cad48fdfbcea0a445f212508459
Jaya Wijono from Dot Kingdom <jayzzwijono@yahoo.com>
2015-07-21 23:57:05 +1000

    resetting the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,11 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     def downcase_email
